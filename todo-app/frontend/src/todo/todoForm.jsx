c1c8540579f1eeebd8c50ae132a94721d3d92342
Rodrigo Estevam <rodrigo.estevam@asbpm.com.br>
2019-05-06 13:59:09 -0300

    icones

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
-
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 export default props => (
-    <div>
-       <h1>Formulario</h1>
+    <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+            <input id='description' className='form-control' placeholder='Adcione uma tarefa' />
+        </Grid>
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='plus'>
+            </IconButton>
+        </Grid>
     </div>
 )
